594eb5157365490d1f34f856cc665a10172fc5f6
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-20 23:26:40 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -2,7 +2,7 @@ class BankAccount
   
   attr_accessor :name, :balance, :status
   
-  def initialize(name)
+  def initialize(name, balance=1000, status)
     @name = name
     @balance = balance
     @status = status
