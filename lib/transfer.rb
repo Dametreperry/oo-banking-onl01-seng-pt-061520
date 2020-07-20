59be2355a802ae2faaa517ef6f873e354e32aa71
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-20 23:49:36 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -2,7 +2,11 @@ class Transfer
   
   attr_accessor :sender, :receiver, :amount, :status
   
-  def initialize()
+  def initialize(sender,receiver,amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+  end
   
   
   
